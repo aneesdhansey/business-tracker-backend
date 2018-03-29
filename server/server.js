1dffa7b11c0ea84af6c3627f9eb38315f60fe0e9
Anees Dhansey <aneesdhansey@gmail.com>
2018-03-29 22:30:13 +0530

    Add GET /users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
+const { authenticate } = require('./middleware/authenticate');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -74,9 +76,9 @@ app.patch('/todos/:id', (req, res) => {
 
     Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
         .then((todo) => {
-            if(!todo)
+            if (!todo)
                 return res.status(404).send();
-            
+
             res.send({ todo });
         })
         .catch((error) => res.status(400).send())
@@ -86,9 +88,15 @@ app.post('/users', (req, res) => {
     const body = _.pick(req.body, ['email', 'password']);
     const user = new User(body);
     user.save()
-            .then(() => user.generateAuthToken())
-            .then((token) => res.header('x-auth', token).send({ user }))
-            .catch((error) => res.status(400).send({ error }));
+        .then(() => user.generateAuthToken())
+        .then((token) => res.header('x-auth', token).send({ user }))
+        .catch((error) => res.status(400).send({ error }));
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
