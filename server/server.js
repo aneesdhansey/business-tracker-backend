6f6ece49c807cad253e03a922bf3834923c59283
Anees Dhansey <aneesdhansey@gmail.com>
2018-03-30 00:17:12 +0530

    Add DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -112,6 +112,12 @@ app.post('/users/login', (req, res) => {
 
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token)
+        .then(() => res.send())
+        .catch(() => res.status(400).send());
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = { app };
